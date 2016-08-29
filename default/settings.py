e78f64a0283bcd072640a41066a8d45bacb1540a
soup <ryo.kubota@topgate.co.jp>
2016-08-29 17:16:07 +0900

    Allocated a client ID for the application, and updated related code to populate a user profile.

--- default/settings.py
+++ default/settings.py
@@ -10,5 +10,5 @@ $Id$
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '878815461434-4fmk4m6g38kn25mqvaufnns2h7ni9rgv.apps.googleusercontent.com'
 
